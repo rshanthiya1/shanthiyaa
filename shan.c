afc25b9b86058b1c75820bcdc8432765d5549907
rshanthiya1 <31534986+rshanthiya1@users.noreply.github.com>
2017-09-02 12:14:46 +0530

    Create shan.c

---
+++ shan.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int x;
+printf("enter the value");
+scanf("%d",&x);
+if(x%2==0)
+{
+printf("even");
+}
+else
+{
+printf("odd");
+}
+getch();
+}
